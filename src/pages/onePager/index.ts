7624e176f8ce687dbccbdf04d76d05f7e5881fb1
ericscottllc <eric@ericscottllc.com>
2025-10-28 09:58:43 -0500

    Updated index.ts

--- src/pages/onePager/index.ts
+++ src/pages/onePager/index.ts
@@ -1,1 +1,14 @@
 export { OnePagerPage } from './OnePagerPage';
+export { OnePagerControls, OnePagerHeader, RegionTable } from './components';
+export { extractAvailableMonths, buildOnePagerData, getRegionMaxByMonth } from './utils/dataProcessing';
+export type { 
+  MasterCrop, 
+  GrainEntry, 
+  OnePagerConfig, 
+  OnePagerData,
+  MasterElevator,
+  MasterTown,
+  MasterRegion,
+  MasterCropComparison,
+  CropClass
+} from './types/onePagerTypes';
